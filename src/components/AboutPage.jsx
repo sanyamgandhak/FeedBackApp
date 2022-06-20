5aa1ef21adc3bca23bb081b81b481b3db13f369f
Sayam_Gandhak <sayam@truscholar.io>
2022-06-21 00:23:47 +0530

    about page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -2,11 +2,38 @@
 
 import Card from './shared/card'
 import {Link} from 'react-router-dom'
+import { FaLinkedin, FaTwitter } from 'react-icons/fa'
 function AboutPage(){
     return (
         <Card className='about-link'>
-           <h1>This is about page </h1>
-           <p>Hi how are you !!</p>
+            <h1>Feedback App</h1>
+           <p>Hi there !! Myself <strong>Sayam Gandhak</strong> . I created this app while learning React.This React App uses Mock Json Server as a Backend . App has following features : - </p>
+           <ul>
+            <strong>
+            <li>Add Feedback</li>
+            <li>Delete Feedback</li>
+            <li>Update Feedback</li>
+            </strong>
+           
+           </ul>
+            <br/>
+           <p>App uses following <strong>React Concepts :-</strong> </p>
+           <ul>
+            <strong>
+            <li>State</li>
+            <li>Component</li>
+            <li>UseEffect</li>
+            <li>Context</li>
+            <li>Props</li>
+            <li>Router</li>
+            </strong>
+            
+           </ul>
+           <br/>
+           <p>You can connect with me here : -<br/>
+            <a href="https://www.linkedin.com/in/sayamgandhak/"> <FaLinkedin></FaLinkedin></a>&nbsp;&nbsp;
+            <a href="https://twitter.com/GandhakSanyam"><FaTwitter></FaTwitter></a></p>
+            <br/>
             <p>
                 <Link to='/'>Back to Home</Link>
             </p>
